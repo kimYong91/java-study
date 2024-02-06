bc0033af9d3c16195264159e51128e8d10df3c05
KimYong91 <rladyd97@naver.com>
2024-02-06 16:34:11 +0900

    Markdown file <2024.02.06>

---
+++ src/개인연습.java
@@ -0,0 +1,12 @@
+public class 개인연습 {
+    public static void main(String[] args) {
+        System.out.println("10*20*50+45=?");
+
+        int a = 10;
+        int b = 20;
+        int c = 50;
+        int d = 45;
+        int result = a * b * c + d;
+        System.out.println(result);
+        }
+    }

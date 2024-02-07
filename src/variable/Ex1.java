9d1d91794359a42f94f9b78f8283b0c1df3934e3
KimYong91 <rladyd97@naver.com>
2024-02-07 12:04:14 +0900

    문제 연습

---
+++ src/variable/Ex1.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        //연습문제 1 : 5와 3을 다른 숫자로 한번에 변경할 수 있게 변수로 선언해주세요
+        int a = 5;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+    }
+}
---
+++ src/variable/Ex4.java
@@ -0,0 +1,36 @@
+package variable;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        /*
+        변수 선언과 초기화:
+        정수 타입의 변수 "number"를 선언하고, 값으로 10을 할당하세요.
+        소수 타입의 변수 "decimal"를 선언하고, 값으로 3.14를 할당하세요.
+        문자 타입의 변수 "letter"를 선언하고, 값으로 'A'를 할당하세요.
+        부울 타입의 변수 "isTrue"를 선언하고, 값으로 true를 할당하세요.
+
+        변수 값 변경:
+        "number" 변수의 값을 5로 변경하고, 변경된 값을 출력하세요.
+        "decimal" 변수의 값을 2.71828로 변경하고, 변경된 값을 출력하세요.
+        "letter" 변수의 값을 'B'로 변경하고, 변경된 값을 출력하세요.
+        "isTrue" 변수의 값을 false로 변경하고, 변경된 값을 출력하세요.
+         */
+        int number = 10;
+        int temp;
+        temp = number;
+        number = 5;
+        System.out.println(number);
+
+        double decimal = 3.14;
+        double d = decimal;
+        d = decimal;
+        System.out.println(d);
+
+        char letter = 'A';
+        boolean isTrue = true;
+
+
+
+
+    }
+}

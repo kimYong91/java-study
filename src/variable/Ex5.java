fe5fdc1996bd36078627f485a40e939649288b7b
KimYong91 <rladyd97@naver.com>
2024-02-07 15:00:15 +0900

    문제 연습

---
+++ src/variable/Ex5.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        /*
+        이름 : 홍길동
+        나이 : 25
+        전화 : 010-1234-5678
+         */
+        String name = "홍길동";
+        int age = 25;
+        String tel1 = "010", tel2 = "1234", tel3 = "4567";
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age);
+        //System.out.println("전화 : " + "010" + "-1234" + "-4567");
+        System.out.print();
+        System.out.printf();
+
+    }
+}

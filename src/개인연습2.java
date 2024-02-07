f67fd4c085021e625ffc810b282204a3af7a3ed2
KimYong91 <rladyd97@naver.com>
2024-02-07 09:28:54 +0900

    개인연습 수업전 <2024.02.07>

--- src/개인연습2.java
+++ src/개인연습2.java
@@ -6,5 +6,10 @@ public class 개인연습2 {
         int b = 24;
         int resurlt = a + b; // resutlt 가 밑줄이 있는 이유는?
         System.out.println(resurlt);
+
+        int c=00;
+        int d=02;
+        int resurlt =c+d; // 계산이 동시에 두 개 가 들어가서 안돼는 건가?
+        System.out.println(resurlt);
     }
 }

4624828052d8f58cd0dd9bd8c08a06f617cba851
KimYong91 <rladyd97@naver.com>
2024-02-07 15:00:45 +0900

    문자열 타입변환 및 입출력

--- src/variable/Read1.java
+++ src/variable/Read1.java
@@ -10,11 +10,7 @@ public class Read1 {
             // 읽은 결과는 KeyCode로 저장됨
             System.out.println(read);
 
-            // 키보드에 입력된 내용을 문자열로 얻기
-            Scanner scanner = new Scanner(System.in);
-            String line = scanner.nextLine();
 
-        System.out.println(line);
         }
 
 

736beff8902da33241cd2a1500d0f01fffe51bde
KimYong91 <rladyd97@naver.com>
2024-02-07 09:11:24 +0900

    개인연습 <2024.02.07>

---
+++ src/개인연습2.java
@@ -0,0 +1,10 @@
+public class 개인연습2 {
+    public static void main(String[] args) {
+        System.out.println("2024.02.07");
+
+        int a = 2000;
+        int b = 24;
+        int resurlt = a + b; // resutlt 가 밑줄이 있는 이유는?
+        System.out.println(resurlt);
+    }
+}

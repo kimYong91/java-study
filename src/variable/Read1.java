4a6016b49890f88b7ed8ac9b0bfec9ceace2c356
KimYong91 <rladyd97@naver.com>
2024-02-07 14:33:05 +0900

    문자열 타입변환 및 입출력

--- src/variable/Read1.java
+++ src/variable/Read1.java
@@ -1,19 +1,20 @@
 package variable;
 
 import java.io.IOException;
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class Read1 {
     public static void main(String[] args) throws IOException {
             int read = System.in.read();
-
+            // 읽은 결과는 KeyCode로 저장됨
             System.out.println(read);
 
-            Scanner Scanner = new Scanner(System.in);
-            Scanner Line = scanner.nextline();
+            // 키보드에 입력된 내용을 문자열로 얻기
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
 
-        System.out.println(Line);
+        System.out.println(line);
         }
 
 

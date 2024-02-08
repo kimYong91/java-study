74cee0bff29d703c7e1155da5bca609aed4d5dc0
KimYong91 <rladyd97@naver.com>
2024-02-08 10:00:46 +0900

    문자열 더하기

---
+++ src/operator/Operator2.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        // 문자열 더하기
+        // 문자열에도 + 연사자를 사용하여 연결할 수 있다.
+        String result1 = "hello" + "java";
+        System.out.println("result1 = " + result1);
+
+        // 문자열과 숫자 더하기
+        String result2 = "a + b = " + 10;
+        System.out.println("result2" + result2);
+    }
+}

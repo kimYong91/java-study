682c34f1ead290754094ff51bcf1e35d68f988c7
rladyd <rladyd97@naver.com>
2024-02-11 19:03:04 +0900

    연습 문제

--- src/condition/Ex7.java
+++ src/condition/Ex7.java
@@ -1,10 +1,20 @@
 package condition;
 
+import java.util.Scanner;
+
 public class Ex7 {
     public static void main(String[] args) {
         /*
          삼항연산자를 사용해서 짝수와 홀수를 구별하는 프로그램을 작성해주세요.
          Hint : num % 2
          */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자 : ");
+        int number = scanner.nextInt();
+
+
+        System.out.println(((number % 2 == 0) ? "짝수" : "홀수") + "입니다.");
+
+
     }
 }

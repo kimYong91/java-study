266d145b11172e4f32ecb9c7fed265b894eb9cf2
KimYong91 <rladyd97@naver.com>
2024-02-14 10:21:28 +0900

    학생 수 가 많을 때이전의 방식으로는 불편함

---
+++ src/array/Array1.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int score1 = 80;
+        int score2 = 70;
+        int score3 = 90;
+        int score4 = 50;
+        int score5 = 40;
+        int score6 = 30;
+
+        System.out.println("학생 점수 : " + score1);
+        System.out.println("학생 점수 : " + score2);
+        System.out.println("학생 점수 : " + score3);
+        System.out.println("학생 점수 : " + score4);
+        System.out.println("학생 점수 : " + score5);
+        System.out.println("학생 점수 : " + score6);
+
+        // 학생수가 증가 하면서 코딩의 양이 비례하면서 증가 -> 반복문도 사용안됨
+    }
+}

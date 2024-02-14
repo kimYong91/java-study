c2bfb5acb29b21ffb6aae43ce8bc540f7db931f3
rladyd <rladyd97@naver.com>
2024-02-14 21:20:42 +0900

    반복문을 이용해서 20이내의 짝수를 출력

--- src/loop/Ex1.java
+++ src/loop/Ex1.java
@@ -5,17 +5,19 @@ public class Ex1 {
         // 반복문을 이용해서 20이내의 짝수를 출력해주세요
 
         for (int i = 1; i <= 20; i++) {
-            /*
-            (int i = 1; i <= 20; i+= 2)
-                 System.out.println(i);
-             */
-            //System.out.println(i * 2);
+            if (i % 2 == 0) {
+                System.out.println("짝수 : " + i);
+
+            }
+
+
+        }
+        for (int i = 1; i <= 20; i++) { // 이 방법이 더 가독성이 좋다고 함
             if (i % 2 != 0){
                 continue;
             }
             System.out.println("짝수 : " + i);
         }
-
 
 
 

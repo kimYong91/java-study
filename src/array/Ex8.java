6d0fc6c542caad21d6889960fc38ee463a802a4d
KimYong91 <rladyd97@naver.com>
2024-02-14 16:06:44 +0900

    이진탐색 binarySearch

--- src/array/Ex8.java
+++ src/array/Ex8.java
@@ -6,17 +6,19 @@ public class Ex8 {
         // 숫자가 정렬된 배열이 있습니다. 배열을 반으로 나누고,
         // 목표값이 있는 방향으로 다시 반으로 나누는 방식으로 목표값을 찾는 알고리즘을
         // 구현해보세요.
+        // 시간복잡도가 O(log n) : 만큼 반복하여 값을 찾는다.
+        // 배열의 크기가 커질수록 선형탐색보다 훨씬 효율적으로 탐색을 한다.
         int[] numbers = {11, 20, 36, 47, 54, 56, 72, 81, 90, 104};
-        int target = 72;
-
+        int target = 56;
 
         int left = 0;       // 가장 왼쪽 인덱스
-        int right = 9;      // 가장 오른쪽 인덱스
+        int right = numbers.length - 1;      // 가장 오른쪽 인덱스
         while (left <= right) {
-        int mid = (left + right) / 2;       // 가운데 인덱스
+            int mid = (left + right) / 2;          // 가운데 인덱스 4
+
             if (numbers[mid] == target) {
-                System.out.println("값을 찾았습니다. 타겟은 " + mid + "번 째 있습니다.");
-                break;
+                System.out.println("값을 찾았습니다. 타겟은 " + (mid + 1) + "번 째 있습니다.");
+                break;  // 값을 찾으면 종료
             } else if (target < numbers[mid]) {
                 right = mid - 1;
             } else {

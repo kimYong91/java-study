57457267029863dfdda31195520bbf6e8a718bd5
rladyd <rladyd97@naver.com>
2024-02-14 23:09:45 +0900

    while 간단한 연습

---
+++ src/개인연습/개인연습16.java
@@ -0,0 +1,15 @@
+package 개인연습;
+
+public class 개인연습16 {
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 10;
+
+        while (start <= end) {
+            if (start % 2 != 0) {
+                System.out.println(start);
+            }
+            start++;
+        }
+    }
+}

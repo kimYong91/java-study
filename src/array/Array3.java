6c2000330751561fa26e71d34e2b7d3325d7e110
KimYong91 <rladyd97@naver.com>
2024-02-14 11:00:41 +0900

    int[] 효율적으로 쓰는법 2

--- src/array/Array3.java
+++ src/array/Array3.java
@@ -4,12 +4,17 @@ public class Array3 {
     public static void main(String[] args) {
 
         int[] scores;       // 배열 변수 선언
-        scores = new int[] {100,90,50,60,70,30};    // 배열 초기화 6개
+        scores = new int[] {100,90,50,60,70,30};    // 배열 초기화, {} <- 안에 적은 것만큼 scores가 생성
 
-        int[] scores2 = {100,90,80}; // 선언과 초기화 동시 가능
+        int[] scores2 = {100,90,80}; // 선언과 초기화 동시 가능, ^ 위의 결과와 같음
 
         for (int i = 0; i < scores.length; i++) {
-            System.out.println(i + 1 + "번 점수 : " + scores[i]);
+            System.out.println(i + 1 + "번 점수 : " + scores[i]); // <- 여기서 1은 첫번째 부터 시작하라는 지시
+                                                                 // i=0으로 시작하기에 0+1= '1' + "번"이라고 출력됨
+                                                                 // 1은 고장값이고 i는 i++ 떄문에 1씩 증가
+                                                                 // i(0)+1 = i(1) "번"
+                                                                 // i(1)+1 = i(2) "번"
+                                                                 // i(2)+1 = i(3) "번" ...
         }
 
         System.out.println(scores);  // [I@506e1b77 라고 뜸

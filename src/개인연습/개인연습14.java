0a1fe6cc4f62b9ad3b95838d30ab0a5c78850960
rladyd <rladyd97@naver.com>
2024-02-14 22:01:45 +0900

    구구단

---
+++ src/개인연습/개인연습14.java
@@ -0,0 +1,13 @@
+package 개인연습;
+
+public class 개인연습14 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                int result = i * j;
+                System.out.println(i + "*" + j + "=" + result);
+            }
+        }
+    }
+}

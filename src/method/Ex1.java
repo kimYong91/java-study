3ef8d2b718a423c9e0eccea1e0fd695f92793293
KimYong91 <rladyd97@naver.com>
2024-02-15 16:35:40 +0900

    메서드 (두 수를 입력받아 뺄셈을 구하기)

--- src/method/Ex1.java
+++ src/method/Ex1.java
@@ -6,16 +6,17 @@ public class Ex1 {
     public static void main(String[] args) {
         // 두 수를 입력받아 뺄셈을 하는 메서드를 만들어주세요
         Scanner scanner = new Scanner(System.in);
-        System.out.print("A : ");
-        int a = scanner.nextInt();
-        System.out.print("B : ");
-        int b = scanner.nextInt();
+        System.out.print("정수 A :");
+        int A = scanner.nextInt();
+        System.out.print("정수 B :");
+        int B = scanner.nextInt();
+        System.out.println("결과 : " + sum(A,B));
 
-        sub(a, b);
-        System.out.println(sub(a, b));
 
     }
-    public static int sub(int a, int b) {
-        return a-b;
-        }
+
+    public static int sum(int a, int b){
+        return a - b;
+    }
+
     }

a62a7abd37bc8d54a776af0ff08ae8de4cfe94a7
KimYong91 <rladyd97@naver.com>
2024-02-15 09:12:10 +0900

    2차원 배열을 활용한 점수 계산 문제, array 5_1문제

---
+++ src/개인연습/개인연습17.java
@@ -0,0 +1,19 @@
+package 개인연습;
+
+import java.util.Scanner;
+
+public class 개인연습17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("학생 수 : ");
+        int students = scanner.nextInt();
+
+        String[] subject = {"수학", "과학", "영어"};
+        int[][] score = new int [students][subject.length];
+
+        for (int i = 0; i < score.length; i++) {
+
+
+        }
+    }
+}

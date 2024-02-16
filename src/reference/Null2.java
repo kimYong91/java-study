eb2556f51897814d07c1bfc034179532f4e33151
KimYong91 <rladyd97@naver.com>
2024-02-16 15:30:16 +0900

    Null 설명

---
+++ src/reference/Null2.java
@@ -0,0 +1,11 @@
+package reference;
+
+public class Null2 {
+    public static void main(String[] args) {
+        Data data = null;
+        data.value1 = 100;
+        // null.value1 = 100; <- 참조할 주소가 존재하지 않는다.
+        // NullPointerException 이 발생하며 프로그램이 종료된다.
+        System.out.println(data.value1);
+    }
+}

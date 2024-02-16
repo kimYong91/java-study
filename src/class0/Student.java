ba1638375dbb352a2817c4c27205b3f6d6dbb09f
KimYong91 <rladyd97@naver.com>
2024-02-16 10:16:55 +0900

    클래스, 인스턴스 설명 및 효과

---
+++ src/class0/Student.java
@@ -0,0 +1,11 @@
+package class0;
+// 클래스 = 설계도 : 객체를 만들기 위한 설계도
+// 인스턴스 : 설계도 대로 생성된 객체, new 연산자로 생성
+public class Student {
+
+    // 필드 : 클래스에 정리한 변수, 데이터 항목을 가리키는 용어, 멤버 변수하고도 부름
+    String name;
+    int score;
+    int age;
+
+}

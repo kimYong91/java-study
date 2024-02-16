5ccbdf69e53dc64aecd6fbf67f7d19ed76356698
KimYong91 <rladyd97@naver.com>
2024-02-16 15:32:51 +0900

    연습문제 미완성

--- src/class0/ex/Ex2.java
+++ src/class0/ex/Ex2.java
@@ -1,46 +1,19 @@
-package class0.ex;
-
-import java.util.zip.ZipFile;
-
 public class Ex2 {
     public static void main(String[] args) {
-         ProductOrder productOrder1;
-         productOrder1 = new ProductOrder();
-         productOrder1.productName = "노트북";
-         productOrder1.price = 1000000;
-         productOrder1.quantity = 2;
-
-         ProductOrder productOrder2;
-         productOrder2 = new ProductOrder();
-         productOrder2.productName = "마우스";
-         productOrder2.price = 15000;
-         productOrder2.quantity = 3;
-
-         ProductOrder productOrder3;
-         productOrder3 = new ProductOrder();
-         productOrder3.productName = "키보드";
-         productOrder3.price = 50000;
-         productOrder3.quantity = 1;
-
-         ProductOrder[] productOrders = new ProductOrder[3];
-         productOrders[0] = productOrder1;
-         productOrders[1] = productOrder2;
-         productOrders[2] = productOrder3;
-
-//        ProductOrder[] productOrders = {productOrder1, productOrder2, productOrder3}; // 위의 것과 같으나 간단함
-
-        int totalPayment = 0;
-
-        for (int i = 0; i < productOrders.length; i++) {
-            System.out.println("상품명 : " + productOrders[i].productName +", 가격 : " + productOrders[i].price +
-                    ", 수량 : " + productOrders[i].quantity + "개");
-
-            int payment = productOrders[i].price * productOrders[i].quantity;
-            totalPayment += payment;
-        }
-
-        // 최종 결제 금액
-        System.out.println("총 결제 금액 : " + totalPayment + "원");
-
+//        Car car1 = new Car();
+//        car1.brand = "삼성";
+//        car1.model = "SM5";
+//        car1.year = 2020;
+//
+//        Car car2 = new Car();
+//        car2.brand = "현대";
+//        car2.model = "아반떼";
+//        car2.year = 2021;
+//
+//        Car[] cars = { car1, car2 };
+//
+//        for (Car car : cars) {
+//            System.out.println("브랜드 : " + car.brand + " 모델 : " +car.model + " 연도: "+ car.year);
+//        }
     }
 }

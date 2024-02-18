940848aa253e2eed4387a11069279d2d817f9664
rladyd <rladyd97@naver.com>
2024-02-18 17:15:00 +0900

    학생수, 점수 계산

--- src/개인연습/개인연습19.java
+++ src/개인연습/개인연습19.java
@@ -6,39 +6,28 @@ import java.util.Scanner;
 
 public class 개인연습19 {
     public static void main(String[] args) {
-         /*
-    반복문 : 짝수의 합 구하기
-    사용자로부터 두 정수 start 와 end 를 입력받아,
-    start 와 end 사이의 모든 짝수의 합을 계산하는 프로그램을 작성하세요
-    */
-    /*
-    출력예시
-    ======
-    시작 정수: 4
-    끝 정수: 15
-    4 + 6 + 8 + 10 + 12 + 14 = 54
-    */
+        /*
+        예시
+        학생수는 몇명인가요? 3
+        점수를 입력하세요
+        10
+        20
+        30
+        >> 합계 : 60, 평균 : 20.0
+         */
         Scanner scanner = new Scanner(System.in);
-        System.out.println("==========");
-        System.out.print("시작 정수 : ");
-        int start = scanner.nextInt();
-        System.out.print("끝 정수 : ");
-        int end = scanner.nextInt();
+        System.out.print("학생 수 는 몇 명 인가요? ");
+        int studentNumber = scanner.nextInt();
+        System.out.println("점수를 입력하세요");
 
         int sum = 0;
 
-        while (start <= end) {
-            if (start % 2 == 0) {
-                sum += start;
-                System.out.print(start);
-                if (start != end)
-                    System.out.print(" + ");
+        for (int i = 0; i < studentNumber; i++) {
+            int studentScore = scanner.nextInt();
 
-            }
-            start++;
+            sum += studentScore;
         }
-        System.out.println();
-        System.out.println("합계 : " + sum);
+        System.out.println(">> 합계 : " + sum + ", 평균 : " + (sum / studentNumber));
 
     }
 

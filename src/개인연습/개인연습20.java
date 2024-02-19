a2e7bd4861e394f4e8d792c4a26ffbf2314094c1
KimYong91 <rladyd97@naver.com>
2024-02-19 09:11:56 +0900

    반복문 : 짝수의 합 구하기

--- src/개인연습/개인연습20.java
+++ src/개인연습/개인연습20.java
@@ -17,22 +17,20 @@ public class 개인연습20 {
     */
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("시작 정수 : ");
+            System.out.print("시작 정수 : ");
             int start = scanner.nextInt();
-            System.out.println("끝 정수 : ");
+            System.out.print("끝 정수 : ");
             int end = scanner.nextInt();
 
-            int totalScore = 0;
+            int totalScore = 0; //  합계를 구해야 하는 상황이라면 일단 무조건 0으로 시작할 바구니를 준비해야함
 
             while (start <= end) {
                 if (start % 2 == 0) {
                     totalScore += start;
-                    if (start != end+1) {
-                        System.out.print(" + ");
-
+                    System.out.print(start); // 순서 중요
+                    if (start < end) {           // 숫자 다음에 +를 하기 위한 코드 end 보다 작을 때만 실행
+                        System.out.print(" + "); // 끝 숫자전까지만 +가 붙음  * (start != end) 이것도 가능
                     }
-                    System.out.print(start);
-
                 }
                 start ++;
             }

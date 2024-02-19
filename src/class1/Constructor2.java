8536ec0da99294ae165bfb74eb747b3cb2fbaa1b
KimYong91 <rladyd97@naver.com>
2024-02-19 16:34:10 +0900

    생성자 응용

--- src/class1/Constructor2.java
+++ src/class1/Constructor2.java
@@ -1,13 +1,15 @@
 package class1;
 
+import java.util.Scanner;
+
 public class Constructor2 {
     public static void main(String[] args) {
         Member member = new Member("홍길동", 30, 20);
         Member member1 = new Member("이순신", 100, 35);
-        Member member2 = new Member(); // 생성자가 존재할 때, 입력값이 없는 생성자를 호출하면 컴파일 오류가 발생 => 런타임 오류를 원천 차단, 제약을 줌
+       // Member member2 = new Member(); // 생성자가 존재할 때, 입력값이 없는 생성자를 호출하면 컴파일 오류가 발생 => 런타임 오류를 원천 차단, 제약을 줌
+        Member member2 = new Member("임꺽정", 40); // 같은 이름의 생성자를 여러개 만들면 여러 변수에 대응 가능
 
-
-        Member[] members = {member, member1};
+        Member[] members = {member, member1, member2};
         for (Member m : members) {
             System.out.println("이름 : " + m.name + " 성적 : " + m.score + " 나이 : " + m.age);
         }
--- src/class1/Member.java
+++ src/class1/Member.java
@@ -1,5 +1,7 @@
 package class1;
 
+import java.util.Scanner;
+
 public class Member {
     String name;    // 이름
     int score;      // 성적
@@ -14,5 +16,17 @@ public class Member {
         this.score = score;
         this.age = age;
     }
-    Member() {} //
+
+    // 생성자 오버로딩 : 매개변수의 개수, 순서가 다르거나, 타입이 다를 경우
+    Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.score = 50;
+    }
+    // 생성자 오버로딩 시 코드를 this()로 줄일 수 있음
+    Member(String name) {
+        // System.out.println("매개변수 한개짜리 생성자 호출");
+        this(name, 0, 0);   // 첫 줄에만 호출 가능
+        // System.out.println("매개변수 한개짜리 생성자 호출"); // 뒷 줄 또한 인식 안함
+    }
 }

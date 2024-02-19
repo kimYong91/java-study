1b96a08496e752c80da573d4f4f735e09b0e7a33
KimYong91 <rladyd97@naver.com>
2024-02-19 13:31:26 +0900

    라디오

---
+++ src/class0/Class8.java
@@ -0,0 +1,19 @@
+package class0;
+
+public class Class8 {
+    public static void main(String[] args) {
+        Radio radio = new Radio();
+
+        radio.turnOn();
+        radio.volumeUp();
+        radio.volumeDown();
+        radio.showRadio();
+        radio.turnOff();
+
+        // 라디오 객체를 생성하고 필요한 행동(메서드)만 호출하면 끝.
+        // 객체를 사용한 쪽에서는 데이터를 몰라도 사용하는데 지장이 없다.
+        // 라디오와 관련된 속성과 기능이 하나의 캡술에 쌓여있는 것 같음 => 캡슐화
+        // 코드를 변경하게 되더라도, 라디오와 관련된 내용을 클래스에서만 변경하면 모두에게 적용
+    }
+
+}
--- src/class0/Radio.java
+++ src/class0/Radio.java
@@ -1,7 +1,41 @@
 package class0;
 
 public class Radio {
-    // 라디오와 관련된
+    // 필드 : 라디오와 관련된 속성, 데이터의 모음
      int volume;
      boolean isOn;
+
+     // 메서드 : 라디오와 관련된 기능, 행동의 모음
+
+
+    // 라디오 켜기 메서드
+      void turnOn () {
+         isOn = true;
+         System.out.println("라디오 전원을 켰습니다.");
+     }
+    // 음량 높이기
+     void volumeUp () {
+        volume++; // 만약 볼륨을 2씩 오르게 하고 싶으면 radio.volume = radio.volume + 2;
+        System.out.println("라디오 음량 : " + volume);
+    }
+    // 음량 줄이기
+     void volumeDown () {
+        volume--;
+        System.out.println("라디오 음량 : " + volume);
+    }
+    // 라디오 상태 확인
+     void showRadio () {
+        System.out.println("라디오 상태 확인");
+        if (isOn == true) {
+            System.out.println("라디오 전원 켜짐, 음량 : " + volume);
+        } else {
+            System.out.println("라디오 전원 꺼짐");
+        }
+    }
+    // 라디오 끄기
+     void turnOff () {
+        isOn = false;
+        System.out.println("라디오를 종료 합니다.");
+    }
+
 }

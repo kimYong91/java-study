5381efb73e197d335eaeca7a69ea23814b2e786a
KimYong91 <rladyd97@naver.com>
2024-02-21 11:30:30 +0900

    Static 설명

---
+++ src/static0/Data.java
@@ -0,0 +1,16 @@
+package static0;
+
+public class Data {
+    //필드
+    private int field;
+
+
+
+    public Data(int field) {
+        this.field = field;
+    }
+
+    public int getField() {
+        return field;
+    }
+}

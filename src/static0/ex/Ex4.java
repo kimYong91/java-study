dce422c62633d5641b4a0424b9d35713ee90eb59
KimYong91 <rladyd97@naver.com>
2024-02-22 09:20:11 +0900

    해봐야 할 것

--- src/static0/ex/Ex4.java
+++ src/static0/ex/Ex4.java
@@ -2,12 +2,7 @@ package static0.ex;
 
 public class Ex4 {
     public static void main(String[] args) {
-        Logger.logInfo("앱이 시작되었습니다...");
-        Logger.logError("에러 메시지입니다.");
 
-        Logger.setLogLevel("ERROR");
-        Logger.logInfo("ERROR 레벨이 세팅되었을 때 출력되는 INFO 메시지입니다.");
-        Logger.logError("ERROR 레벨이 세팅되었을 때 출력되는 ERROR 메시지입니다.");
     }
 
 }

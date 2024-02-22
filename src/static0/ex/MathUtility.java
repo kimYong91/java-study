6f86262f1ae63d2570cc7dd3731d75d49b2edfda
KimYong91 <rladyd97@naver.com>
2024-02-22 09:19:12 +0900

    간단 계산

--- src/static0/ex/MathUtility.java
+++ src/static0/ex/MathUtility.java
@@ -28,7 +28,7 @@ public class MathUtility {
 
     public static int factorial(int n) {
         int result = 1;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             result *= i;
         }
         return result;

cb79fa4226698cc7cbc748ce503c676a960aa7a4
KimYong91 <rladyd97@naver.com>
2024-02-26 09:14:05 +0900

    아직 이해 못함

---
+++ src/개인연습/개인연습_다양한_별만들기.java
@@ -0,0 +1,12 @@
+package 개인연습;
+
+public class 개인연습_다양한_별만들기 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.println("*");
+            }
+        }
+    }
+}

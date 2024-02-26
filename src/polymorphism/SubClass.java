0471042eeb08a2eb4c30b8b6554f38938b87bca4
KimYong91 <rladyd97@naver.com>
2024-02-26 10:31:39 +0900

    자식 클래스

--- src/polymorphism/SubClass.java
+++ src/polymorphism/SubClass.java
@@ -2,6 +2,6 @@ package polymorphism;
 
 public class SubClass extends SuperClass{
     public void subMethod() {
-        System.out.println("서브 클래스의 메서드");
+        System.out.println("자식 클래스의 메서드");
     }
 }

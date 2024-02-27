3e4bb89e0aa1b8a51e4d1d2e87a86e4c7391a8eb
KimYong91 <rladyd97@naver.com>
2024-02-27 15:29:36 +0900

    객체지향 프로그래밍 OOP 5대 원칙 (SOLID) ISP 적용 전

---
+++ src/solid/isp/before/MultiFunctionPrinter.java
@@ -0,0 +1,9 @@
+package solid.isp.before;
+
+// ISP 인터페이스 분리 원칙 미적용
+// 단순한 출력 기능만 있는 프린터도 모든 기능 구현이 강제된다.
+public interface MultiFunctionPrinter {
+    void print ();
+    void scan();
+    void fax();
+}
---
+++ src/solid/isp/before/SimplePrinter.java
@@ -0,0 +1,19 @@
+package solid.isp.before;
+
+public class SimplePrinter implements MultiFunctionPrinter{
+    // 프린터 기능만 있는 단순 프린터
+    @Override
+    public void print() {
+        // 프린트 로직
+    }
+
+    @Override
+    public void scan() {
+        System.out.println("스캔 기능은 되지 않습니다.");
+    }
+
+    @Override
+    public void fax() {
+        System.out.println("팩스 기능은 되지 않습니다.");
+    }
+}

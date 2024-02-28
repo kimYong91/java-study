85b348681c9e6407d393bcd5a64b6ec4b4a19d2e
KimYong91 <rladyd97@naver.com>
2024-02-28 16:41:00 +0900

    중첩 클래스 개인연습

--- src/개인연습/중첩_클래스NestedClass/Franchise.java
+++ src/개인연습/중첩_클래스NestedClass/Franchise.java
@@ -1,9 +1,32 @@
 package 개인연습.중첩_클래스NestedClass;
 
 public class Franchise {
-    private String title;
+    public static class TaskForce {
+        private String title;
+        private String name;
 
-    public Franchise(String title) {
-        this.title = title;
+        public TaskForce(String title, String name) {
+            this.name = name;
+            this.title = title;
+        }
+
+        public Franchise launch() {
+            System.out.println(name + "님께서 " + title + "점 런칭하였습니다.");
+            return new Franchise();
+        }
+    }
+
+    class Coupon {
+        private int amount;
+        private String name;
+
+        public Coupon(int amount, String name) {
+            this.amount = amount;
+            this.name = name;
+        }
+    }
+
+    public void getCoupon(int amount, TaskForce taskForce) {
+        System.out.println("%d원 쿠폰 %s점에서 %s님에게 발급되었습니다.".formatted(amount, taskForce.title, taskForce.name));
     }
 }
--- src/개인연습/중첩_클래스NestedClass/FranchiseMain.java
+++ src/개인연습/중첩_클래스NestedClass/FranchiseMain.java
@@ -8,4 +8,42 @@ public class FranchiseMain {
 
     쿠폰 발행
      */
+    public static void main(String[] args) {
+        Franchise.TaskForce taskForce = new Franchise.TaskForce("남천", "홍길동");
+        taskForce.launch();
+        Franchise namchunStore = taskForce.launch();
+
+        Franchise.TaskForce taskForce1 = new Franchise.TaskForce("금련산", "임꺽정");
+        taskForce1.launch();
+        Franchise kumryuStore = taskForce1.launch();
+
+        namchunStore.getCoupon(10000, taskForce);
+
+    }
+
+    /*
+    Franchise.TaskForce taskForce1 = new Franchise.TaskForce("남천", "홍길동");
+        Franchise namchunStore = taskForce1.launch();
+
+        Franchise.TaskForce taskForce2 = new Franchise.TaskForce("금련산", "임꺽정");
+        Franchise kumryuStore = taskForce2.launch();
+
+        // 2. (인스턴스) 내부 클래스 : 인스턴스가 있어야 생성 가능
+        namchunStore.getCoupon(50000,"신사임당");
+        kumryuStore.getCoupon(10000, "세종대왕");
+
+        Franchise.Coupon coupon = namchunStore.new Coupon(1000, "율곡이이");
+
+
+
+리더 홍길동님을 주축으로 남천TF팀 출범!
+남천점 런칭하였습니다.
+리더 임꺽정님을 주축으로 금련산TF팀 출범!
+금련산점 런칭하였습니다.
+50000원 쿠폰 남천점에서 신사임당님에게 발금되었습니다.
+10000원 쿠폰 금련산점에서 세종대왕님에게 발금되었습니다.
+1000원 쿠폰 남천점에서 율곡이이님에게 발금되었습니다.
+     */
+
+
 }

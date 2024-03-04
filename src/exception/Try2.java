7f9e2caf92cd35c3da93b72b5fc76eeb4e7ee5d7
KimYong91 <rladyd97@naver.com>
2024-03-04 13:43:05 +0900

    예외 클래스 try-catch-finally

--- src/exception/Try2.java
+++ src/exception/Try2.java
@@ -8,7 +8,7 @@ public class Try2 {
         } catch (ArithmeticException e) {
             System.out.println("0으로 나눌 수 없습니다.");   // try안에 있는 코드중 catch () 가로 안에 있는 에러가 발생시 작동
             System.out.println(e.getMessage()); // 에러 관련 메시지를 남겨줌 * getMessage() 이미 자바에서 만든 메서드
-            e.printStackTrace();        // 예외의 stack을 추적해서 출력 * printStackTrace() 이미 자바에서 만든 메서드
+            e.printStackTrace();        // 예외의 stack을 추적해서 붉은 글씨로 출력 * printStackTrace() 이미 자바에서 만든 메서드
             System.out.println("예외 처리 되었습니다.");
         } finally {
             System.out.println("항상 출력되는 부분");

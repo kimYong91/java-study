ac6fe92d6aea60a111d9e23b74534d1bb9e46045
KimYong91 <rladyd97@naver.com>
2024-03-11 12:25:17 +0900

    제네릭 : 결정되지 않은 타입을 파라미터로 처리하고, 실제 사용할 때 파라미터를 구체적인 타입으로 대체하는 기능

---
+++ src/generic/Generic5.java
@@ -0,0 +1,39 @@
+package generic;
+
+public class Generic5 {
+    // 제네릭 인터페이스
+    // 정의: 인터페이스 이름 뒤에 <T> 와 같이 매개변수 추가하여 정의
+    // 제네릭 인터페이스를 구현하는 클래스는 T에 대한 구체적 타입 제공 필요
+
+    interface Container<T> {
+        void put(T item);
+        T get();
+    }
+
+    static class GenericContainer<T> implements Container<T>{
+
+        T item;
+
+        @Override
+        public void put(T item) {
+            this.item = item;
+        }
+
+        @Override
+        public T get() {
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Container<Integer> integerContainer = new GenericContainer<>();
+        integerContainer.put(100);
+        Integer i = integerContainer.get();
+        System.out.println("i = " + i);
+
+        Container<String> stringContainer = new GenericContainer<>();
+        stringContainer.put("문자열");
+        String s = stringContainer.get();
+        System.out.println("s = " + s);
+    }
+}

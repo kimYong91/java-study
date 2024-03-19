fdc5f834d3f79ed710af120b8b0ec6a308f00246
KimYong91 <rladyd97@naver.com>
2024-03-19 09:05:29 +0900

    람다식 연습 문제

--- src/lambda/ex/ex5/Ex5.java
+++ src/lambda/ex/ex5/Ex5.java
@@ -3,10 +3,8 @@ package lambda.ex.ex5;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
+
 
 public class Ex5 {
     public static void main(String[] args) {
@@ -23,7 +21,7 @@ public class Ex5 {
         // 리스트의 각요소에 (Consumer.accept()) 적용
         attendees.forEach(welcome);
 
-        Predicate<Person> isAdult = person -> {return person.getAge() >= 18;};
+        Predicate<Person> isAdult = person -> {return person.getAge() >= 18;}; // 한 줄 짜리기 때문에 리턴, 중괄호는 필요 없어짐
 
         for (Person attendee : attendees) {
             if (isAdult.test(attendee)) {

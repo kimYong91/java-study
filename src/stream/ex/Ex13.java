0e54154bc98fce51382089224654a67ef064ff0f
KimYong91 <rladyd97@naver.com>
2024-03-21 12:23:07 +0900

    도시별 인구수 집계하기

--- src/stream/ex/Ex13.java
+++ src/stream/ex/Ex13.java
@@ -1,6 +1,31 @@
 package stream.ex;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Ex13 {
+    public static void main(String[] args) {
+        record CityPopulation(String city, int population) {}
+
+        List<CityPopulation> cityPopulations = Arrays.asList(
+                new CityPopulation("New York", 8000000),
+                new CityPopulation("Los Angeles", 4000000),
+                new CityPopulation("Chicago", 2700000),
+                new CityPopulation("New York", 7500000),
+                new CityPopulation("Los Angeles", 3800000),
+                new CityPopulation("Chicago", 2600000)
+        );
+
+        Map<String, Double> populationByCity = cityPopulations.stream()
+                .collect(Collectors.groupingBy(
+                        CityPopulation::city, Collectors.averagingInt(CityPopulation::population)
+                ));
+
+        populationByCity.forEach((city,population) -> System.out.printf("%s의 인구 : %,d명\n" , city, population.intValue()));
+    }
 }
 /*
     연습 문제: 도시별 인구수 집계하기

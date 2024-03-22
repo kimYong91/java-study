4ac15f6bf71d54193cb76d7d9f25fc625ad2ed3f
KimYong91 <rladyd97@naver.com>
2024-03-22 14:29:27 +0900

    wait(), notify(), notifyAll()

--- src/thread/Thread13.java
+++ src/thread/Thread13.java
@@ -14,7 +14,7 @@ public class Thread13 {
         Thread thread1 = new Thread(() -> {
             synchronized (lock) {
                 System.out.println("스레드 1 : lock을 쓸 수 있을 떄까지 대기합니다.");
-                // Runnable -> Waiting
+                // Runnable -> Waiting -> Terminate
                 try {
                     lock.wait();
                 } catch (InterruptedException e) {
@@ -27,6 +27,7 @@ public class Thread13 {
 
         Thread thread2 = new Thread(() -> {
             // 스레드 1이 대기 상태로 들어가고 스레드 2가 lock(공유객채)을 사용할 수 있게 됨
+            // Runnable -> Block -> Runnable -> Terminate
             synchronized (lock) {
                 System.out.println("스레드 2 : lock 객체 다 썼음을 알립니다.");
                 // lock 객체를 기다리고 있는 대기중인 스레드에세 알림
@@ -42,5 +43,6 @@ public class Thread13 {
             throw new RuntimeException(e);
         }
         thread2.start();
+        // 모든 스레드가 Terminate 되면 main 스레드도 종료
     }
 }

a5e9709791d70c73d6a57c9b8196d258760977c4
KimYong91 <rladyd97@naver.com>
2024-03-26 16:32:06 +0900

    바이트 출력 스트림을 생성

--- src/io_stream/IOStream1.java
+++ src/io_stream/IOStream1.java
@@ -1,11 +1,11 @@
-package io_stram;
+package io_stream;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class IoStream1 {
+public class IOStream1 {
     // 입출력 스트림
     public static void main(String[] args) {
         try {

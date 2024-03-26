474b56a4150b9618475338367e7f27a4be82eedf
KimYong91 <rladyd97@naver.com>
2024-03-26 13:33:52 +0900

    1 바이트씩 읽기

---
+++ src/io_stram/IoStream3.java
@@ -0,0 +1,31 @@
+package io_stram;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IoStream3 {
+    public static void main(String[] args) {
+        // 1 바이트씩 읽기
+        try {
+            InputStream inputStream = new FileInputStream("src/io_stram/text_IoStream1.txt");
+
+            // 1바이트씩 읽음
+            int read1 = inputStream.read();
+            int read2 = inputStream.read();
+            int read3 = inputStream.read();
+            // 더이상 읽을 바이트가 없으면 -1을 반환
+            int read4 = inputStream.read();
+
+            System.out.println("read1 = " + read1);
+            System.out.println("read2 = " + read2);
+            System.out.println("read3 = " + read3);
+            System.out.println("read4 = " + read4);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

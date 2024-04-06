eb3bb707626e7702059e24e51ba62d121daa8c92
rladyd <rladyd97@naver.com>
2024-04-07 00:31:48 +0900

    04.07

--- src/quiz/quiz7/Quiz2.java
+++ src/quiz/quiz7/Quiz2.java
@@ -9,12 +9,18 @@ import java.util.concurrent.Executors;
 public class Quiz2 {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        List arrayList = new ArrayList<>(Arrays.asList(
+        ArrayList<CoffeeOrder> coffeeOrders = new ArrayList<>(Arrays.asList(
                 new CoffeeOrder("에스프레소", 2),
                 new CoffeeOrder("아메리카노", 3),
                 new CoffeeOrder("카푸치노", 5),
                 new CoffeeOrder("라떼", 4)
         ));
+
+        for (CoffeeOrder coffeeOrder : coffeeOrders) {
+            executorService.execute(coffeeOrder);
+        }
+
+        executorService.shutdown();
 
     }
     static class CoffeeOrder implements Runnable{
@@ -39,3 +45,30 @@ public class Quiz2 {
         }
     }
 }
+/*
+스레드풀 카페에서의 커피 주문 처리
+
+    여러 손님들이 동시에 커피를 주문할 수 있으며, 각 커피를 만드는 데에는 일정 시간이 소요됩니다. 여러 손님의 주문을 효율적으로 처리하기 위해 스레드 풀(직원)을 사용하여 커피 주문을 병렬로 처리하는 시스템을 구현해야 합니다.
+
+    요구사항:
+    - 손님의 주문은 CoffeeOrder 태스크로 표현됩니다. 각 CoffeeOrder는 커피의 이름과 준비 시간(초 단위)을 가집니다.
+    - 스레드 풀을 사용하여 여러 개의 CoffeeOrder를 동시에 처리합니다. 스레드 풀의 크기는 3으로 설정합니다.
+    - 각 주문이이 처리될 때, 콘솔에는 주문받은 커피의 이름과 "준비 완료!"가 출력되어야 합니다
+
+    입력데이터 예시:
+            new CoffeeOrder("에스프레소", 2),
+            new CoffeeOrder("아메리카노", 3),
+            new CoffeeOrder("카푸치노", 5),
+            new CoffeeOrder("라떼", 4),
+
+    출력예시
+    ===
+    pool-1-thread-2> 아메리카노 준비 시작.
+    pool-1-thread-3> 카푸치노 준비 시작.
+    pool-1-thread-1> 에스프레소 준비 시작.
+    pool-1-thread-1> 에스프레소 준비 완료!
+    pool-1-thread-1> 라떼 준비 시작.
+    pool-1-thread-2> 아메리카노 준비 완료!
+    pool-1-thread-3> 카푸치노 준비 완료!
+    pool-1-thread-1> 라떼 준비 완료!
+ */
--- src/quiz/quiz7/Quiz4.java
+++ src/quiz/quiz7/Quiz4.java
@@ -7,18 +7,40 @@ public class Quiz4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("텍스트를 입력하세요 ('종료' 입력 시 프로그램 종료):");
-        while (true) {
-            System.out.print("> ");
-            String text = scanner.nextLine();
-            String file = "src/quiz/quiz7/output.txt";
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            if (text.equals("종료")) return;
+        String file = "src/quiz/quiz7/output.txt";
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            while (true) {
+                System.out.print("> ");
+                String text = scanner.nextLine();
+                if (text.equals("종료")) return;
                 bufferedWriter.write(text);
                 bufferedWriter.newLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                bufferedWriter.flush();
             }
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
+/*
+입출력 스트림, 사용자 입력 문자열을 파일에 저장하기
+
+    사용자로부터 문자열 입력을 받아 이를 "output.txt"라는 이름의 파일에 저장하는 프로그램을 작성하세요.
+
+    요구 사항
+    - 사용자 입력 받기: 사용자로부터 문자열을 계속해서 입력 받으세요. 입력은 터미널(콘솔)을 통해 이루어집니다.
+    - 파일에 저장: 사용자가 입력한 문자열을 "output.txt" 파일에 저장하세요. 사용자가 새 문자열을 입력할 때마다, 이 문자열은 파일의 새로운 줄에 추가되어야 합니다.
+    - 프로그램 종료 조건: 사용자가 "종료"라고 입력하면, 프로그램은 더 이상 입력을 받지 않고 종료됩니다. 이때, 지금까지 입력받은 모든 문자열이 "output.txt" 파일에 저장되어 있어야 합니다.
+
+    실행예시
+    ===
+    텍스트를 입력하세요 ('종료' 입력 시 프로그램 종료):
+    > 안녕하세요
+    > 파일 입출력 연습입니다.
+    > 종료
+
+    output.txt
+    ===
+    안녕하세요
+    파일 입출력 연습입니다.
+ */

6af8bfd6ca93f1c08d2e12a09488a10ff9327d31
KimYong91 <rladyd97@naver.com>
2024-04-22 13:33:09 +0900

    try-with-resources 구문으로 자원 정리하기

---
+++ src/jdbc/Jdbc4.java
@@ -0,0 +1,30 @@
+package jdbc;
+
+import java.sql.*;
+import java.util.Date;
+
+public class Jdbc4 {
+    // try-with-resources 구문으로 자원 정리하기
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/scott";
+        String user = "root";
+        String password = "1234";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);    // 1. 연결
+             Statement stmt = conn.createStatement();                               // 2. 쿼리 생성
+             ResultSet rs = stmt.executeQuery("SELECT * FROM emp")) {           // 2. 실행
+
+            // 4. 결과 처리
+            while (rs.next()) {
+                int eno = rs.getInt(1);     // 첫번째 컬럼을 정수타입으로 반환
+                String ename = rs.getString(2); // 두번째 컬럼을 문자열 타입으로
+                Date hiredate= rs.getDate("hireDate");
+                System.out.println("사원번호: " + eno + ", 이름: " + ename + ", 입사일: " + hiredate);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } // 5. 자원 종료
+        // close 메서드 사용하지 않아도, try 구문이 끝나면 객체를 안전하게 종료함.
+    }
+}

dcd4e125eaa3b9b379461faa694df753e33d4893
KimYong91 <rladyd97@naver.com>
2024-04-26 15:09:51 +0900

    04.26

--- src/static0/ex/MathArrayUtil.java
+++ src/static0/ex/MathArrayUtil.java
@@ -1,15 +1,21 @@
 package static0.ex;
 
+/**
+ * 더하기, 평균, 최소,최대 값 구하는 클래스
+ */
 public class MathArrayUtil {
-//    sum(int[] array) : 배열의 모든 요소를 더하여 합계를 반환합니다.
-//    average(int[] array) : 배열의 모든 요소의 평균값을 계산합니다.
-//            min(int[] array) : 배열에서 최소값을 찾습니다.
-//            max(int[] array) : 배열에서 최대값을 찾습니다.
 
-
+    /**
+     * 생성불가 생성자
+     */
     private MathArrayUtil() {
     }
 
+    /**
+     * 배열의 모든 요소를 더하여 합계를 반환합니다.
+     * @param array 더하고 싶은 숫자 입력
+     * @return 더한 결과 값
+     */
     public static int sum(int[] array) {
         int total = 0;
         for (int num : array) {
@@ -18,10 +24,20 @@ public class MathArrayUtil {
         return total;
     }
 
+    /**
+     * 배열의 모든 요소의 평균값을 계산합니다.
+     * @param array 평균을 구하고 싶은 숫자 입력
+     * @return 평균치 결과 값
+     */
     public static double average(int[] array) {
         return sum(array) / array.length;
     }
 
+    /**
+     * 배열에서 최소값을 찾습니다.
+     * @param array 최소값을 구하고 싶은 숫자
+     * @return 최소 값
+     */
     public static int min(int[] array) {
         int min = array[0];
         for (int num : array) {
@@ -32,6 +48,11 @@ public class MathArrayUtil {
         return min;
     }
 
+    /**
+     * 배열에서 최대값을 찾습니다.
+     * @param array 최대값을 구하고 싶은 숫자
+     * @return 최대 값
+     */
     public static int max(int[] array) {
         int max = array[0];
         for (int num : array) {
--- src/static0/ex/MathUtility.java
+++ src/static0/ex/MathUtility.java
@@ -1,12 +1,17 @@
 package static0.ex;
 
+    /**
+    * 유틸리티 클래스 : 필드 없음
+    */
 public class MathUtility {
-    // 유틸리티 클래스 : 필드 없음
-//
-//    static 메소드로 최대값을 찾는 max(int a, int b) 메소드를 구현합니다.
-//    static 메소드로 최소값을 찾는 min(int a, int b) 메소드를 구현합니다.
-//    static 메소드로 두 수의 합을 계산하는 sum(int a, int b) 메소드를 구현합니다.
-//    static 메소드로 팩토리얼을 계산하는 factorial(int n) 메소드를 구현합니다. 팩토리얼은 n!로 표현되며, 0! = 1입니다.
+
+
+    /**
+     * static 메소드로 최대값을 찾는 max(int a, int b) 메소드를 구현합니다.
+     * @param a 숫자
+     * @param b 숫자
+     * @return 최대 값
+     */
     public static int max(int a, int b) {
         if (a > b) {
             return a;
@@ -15,14 +20,33 @@ public class MathUtility {
         }
     }
 
+        /**
+         * static 메소드로 최소값을 찾는 min(int a, int b) 메소드를 구현합니다.
+         * @param a
+         * @param b
+         * @return
+         */
     public static int min(int a, int b) {
         return a < b ? a : b;
     }
 
+        /**
+         *
+         * @param a
+         * @param b
+         * @return
+         */
     public static int sum(int a, int b) {
         return a + b;
     }
 
+        /**
+         * static 메소드로 팩토리얼을 계산하는 factorial(int n) 메소드를 구현합니다.
+         * 팩토리얼은 n!로 표현되며, 0! = 1입니다.
+         *
+         * @param n 팩토이얼을 구할 숫자
+         * @return 팩토리얼 결과 값
+         */
     public static int factorial(int n) {
         int result = 1;
         for (int i = n; i >= 1; i--) {
